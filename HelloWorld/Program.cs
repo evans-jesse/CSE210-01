5c375510caf7237767e955c1b69a452f13b40e17
Jesse Evans <eva20005@byui.edu>
2022-01-10 12:50:33 -0700

    New test

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,9 +6,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            string y = "Hello";
-            Console.WriteLine("This is a test - " + y);
-            string x = Console.ReadLine();
+            Console.WriteLine("Please Enter you're name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ", welcome to class.");
+            
+            // string y = "Hello";
+            // Console.WriteLine("This is a test - " + y);
+            // string x = Console.ReadLine();
         }
     }
 }
