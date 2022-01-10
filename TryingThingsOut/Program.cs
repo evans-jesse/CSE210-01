9119e8e80e55210b61e0040ccd8fc856a4e58e7f
Jesse Evans <eva20005@byui.edu>
2022-01-10 12:56:00 -0700

    Got the TryingThingsOut to hello world

--- TryingThingsOut/Program.cs
+++ TryingThingsOut/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace TryingThingsOut {
+    class Project
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
