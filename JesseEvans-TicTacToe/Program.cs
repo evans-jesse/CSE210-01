ff42cf3a063bec86af0768a7a8861eacd48078fe
Jesse Evans <eva20005@byui.edu>
2022-01-20 01:06:58 -0700

    Finished TicTacToe

--- JesseEvans-TicTacToe/Program.cs
+++ JesseEvans-TicTacToe/Program.cs
@@ -10,6 +10,12 @@ Tic-Tac-Toe is played according to the following rules.
 5. If all nine squares are full and neither player has three in a row, the game ends in a draw.
 */
 
+
+/*
+* @author Jesse Evans
+* @assignment Tic Tac Toe
+*/
+
 namespace JesseEvans_TicTacToe
 {
     class Program
@@ -17,25 +23,75 @@ namespace JesseEvans_TicTacToe
         static void Main(string[] args)
         {
             int turn = 1;
-            String turnLetter;
-            String response;
+            string turnLetter;
+            string response;
             string[] array = new string[] {"1","2","3","4","5","6","7","8","9"};
 
-            BuildBoard(array);
+            do{
+                response = "";
+                BuildBoard(array);
+
+                if(turn % 2.0 == 0){
+                    turnLetter = "O";
+                }
+                  else {
+                    turnLetter = "X";
+                }
+
+                Console.Write(turnLetter + "s turn to choose a square (1-9): ");
+                response = Console.ReadLine();
+                Console.WriteLine();
+
+                int item = (Convert.ToInt32(response)) - 1;
+                if(array[item] == "X" || array[item] == "O"){
+                    Console.WriteLine("That spot has already been taken.");
+                    Console.WriteLine();
+                    continue;  // Makes sure the turn doesn't increase.
+                }
+                  else {
+                    array[item] = turnLetter;
+                }
+
+                if(winStatus(array)){
+                    BuildBoard(array);
+                    Console.WriteLine("Good Game.  " + turnLetter + " won the game.  Thanks for playing!");
+                    turn = 100;
+                }
+
+                if(turn == 9){
+                    BuildBoard(array);
+                    Console.WriteLine("Game Over.  No Winner.");
+                }
+                  turn++;
+
+            } while(turn < 10);
+            
         }
         static void BuildBoard(string[] board)
         {
             Console.WriteLine(board[0] + "|" + board[1] + "|" + board[2]);
             Console.WriteLine("-+-+-");
             Console.WriteLine(board[3] + "|" + board[4] + "|" + board[5]);
-            // Console.WriteLine("4|5|6");
             Console.WriteLine("-+-+-");
-            // Console.WriteLine("7|8|9");
             Console.WriteLine(board[6] + "|" + board[7] + "|" + board[8]);
         }
-        static void gameplay(string[] temp)
-        {
-            
+        static bool winStatus(string[] gameBoard){
+            if(   (gameBoard[0] == gameBoard[1] && gameBoard[1] == gameBoard[2])  ||
+                  (gameBoard[3] == gameBoard[4] && gameBoard[4] == gameBoard[5])  ||
+                  (gameBoard[6] == gameBoard[7] && gameBoard[7] == gameBoard[8])  ||
+
+                  (gameBoard[0] == gameBoard[3] && gameBoard[3] == gameBoard[6])  ||
+                  (gameBoard[1] == gameBoard[4] && gameBoard[4] == gameBoard[7])  ||
+                  (gameBoard[2] == gameBoard[5] && gameBoard[5] == gameBoard[8])  ||
+
+                  (gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8])  ||
+                  (gameBoard[2] == gameBoard[4] && gameBoard[4] == gameBoard[6])  ) {
+                 
+                 return true;
+            }
+            else {
+                return false; 
+            } 
         }
     }
 }
